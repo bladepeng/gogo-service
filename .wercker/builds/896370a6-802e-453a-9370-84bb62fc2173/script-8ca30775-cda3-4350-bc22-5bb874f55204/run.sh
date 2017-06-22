a6e841c6c40e9c07f79e4a9264ea3e5cbeb6c8b1
Jimmy Song <rootsongjc@gmail.com>
2017-06-22 22:29:51 +0800

    删除无用包

--- .wercker/builds/896370a6-802e-453a-9370-84bb62fc2173/script-8ca30775-cda3-4350-bc22-5bb874f55204/run.sh
+++
@@ -1,7 +0,0 @@
-set -e
-cd $WERCKER_SOURCE_DIR
-go version
-go get -u github.com/Masterminds/glide
-go get -u github.com/cloudnativego/cf-tools/vcapinate
-export PATH=$WERCKER_SOURCE_DIR/bin:$PATH
-glide install
--- .wercker/builds/896370a6-802e-453a-9370-84bb62fc2173/script-b23b9968-78c9-4f65-a940-e7b9969ed96a/run.sh
+++
@@ -1,4 +0,0 @@
-set -e
-export VCAP_SERVICES=`vcapinate -path=./integrations/vcap.yml`
-export VCAP_APPLICATION={}
-go test ./integrations/_test -v -race
